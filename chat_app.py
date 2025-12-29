8ad3d56f445d56c19a47b6b2993f8306fee7004d
itsanshika8345-tech <itsanshika8345@gmail.com>
2025-12-29 15:53:15 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -67,9 +67,10 @@ if user_prompt:
 if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.chat = model.start_chat(history=[])
     st.session_state.messages = []
-    st.experimental_rerun()
+    st.rerun()
 
 st.markdown('**Design and developed by: Anshika**')
+
 
 
 
